c2c87c2201fb5c578bdaf28e96426e8feaccc634
aninha2805 <160134732+aninha2805@users.noreply.github.com>
2024-11-25 12:17:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Uma lingagem de programaçao'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'A capital da Argentina é Buenos Aires'
+)
+
+criaCartao(
+    'como passar de ano?',
+    '?',
+    'Quero descobrir também'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
